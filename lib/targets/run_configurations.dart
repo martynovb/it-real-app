a097c72a544defd03e4fe19ec6f8dbc71ec1d74f
Bohdan Martynov <48797124+martynovb@users.noreply.github.com>
2024-04-04 19:52:42 +0300

    add crashlytics (#3)

--- lib/targets/run_configurations.dart
+++ lib/targets/run_configurations.dart
@@ -1,8 +1,13 @@
 class RunConfigurations {
+  // Supabase
   static String supabaseAnonKey = const String.fromEnvironment(
     'supabaseAnonKey',
   );
   static String supabaseUrl = const String.fromEnvironment(
     'supabaseUrl',
+  );
+
+  static String sentryDsn = const String.fromEnvironment(
+    'sentryDsn',
   );
 }
